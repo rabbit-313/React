90bf96566d98ccbd55ede1ce31db11e2aee2dff2
rabbit-313 <ru.ito@zozo.com>
2024-07-18 15:20:31 +0900

    domの取得

---
+++ Study/Dom/src/index.js
@@ -0,0 +1,11 @@
+const title1 = document.getElementById("title")
+console.log(title1)
+
+const title2 = document.querySelector("#title")
+console.log(title2)
+
+const containers = document.getElementsByClassName("container")
+console.log(containers)
+
+const container = document.querySelector(".container")
+console.log(container)
