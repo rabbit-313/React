8d2c6776f9395c57025e72b45619a309cf2e478c
rabbit-313 <migiso1051@i.softbank.jp>
2024-07-19 23:40:34 +0900

    add:props

--- Study/react-app/src/App.jsx
+++ Study/react-app/src/App.jsx
@@ -1,17 +1,15 @@
+import { ColoredMessage } from "./components/ColoredMessage";
 export const App = () => {
   const onClickButton = () => {
     alert();
   };
 
-  const contentStyle = {
-    color: "blue",
-    fontSize: "20px",
-  };
-
   return (
     <>
       <h1 style={{ color: "red" }}> Hello World</h1>
-      <p style={contentStyle}> This is a start line for my dream</p>
+      <ColoredMessage />
+      <ColoredMessage color="blue">you good?</ColoredMessage>
+      <ColoredMessage color="pink">I'm good</ColoredMessage>
       <button onClick={onClickButton}> Click</button>
     </>
   );
